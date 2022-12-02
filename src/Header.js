ee72a056bb0396c18e34ef77ac3e631c5df879d9
vv411 <vv411@hotmail.com>
2022-12-02 11:23:01 -0500

    Adding MUI Snackbar - initial implementation

--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import Box from '@mui/material/Box';
 import AppBar from '@mui/material/AppBar';
@@ -7,10 +7,32 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
+import Snackbar from '@mui/material/Snackbar';
 
 import { createMockFormSubmission } from './service/mockServer';
 
 export default function Header() {
+
+  const [open, setOpen] = useState(true);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const action = (
+    <React.Fragment>
+        <IconButton
+          size="small"
+          aria-label="close"
+          color="inherit"
+          onClick={handleClose}
+        >
+        <CloseIcon fontSize="small" />
+        </IconButton>
+    </React.Fragment>
+    );
+
   return (
     <Box sx={{flexGrow: 1}}>
       <AppBar position="static">
@@ -36,6 +58,15 @@ export default function Header() {
           </Button>
         </Toolbar>
       </AppBar>
+
+      <Snackbar
+        open={open}
+        autoHideDuration={10000}
+        close={handleClose}
+        message={"test"}
+        action={action}
+      />
+
     </Box>
   );
 }
